0c2bcaf4609d44cb0721d49d265989bd6c749ec0
lvquang <levinhquang1412@gmail.com>
2022-10-02 07:33:24 +0700

    add svgr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
+    images: {
+        domains: ["source.unsplash.com"],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
