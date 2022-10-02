cde023a5c59ae3c696a1cc74ae509d61718d6612
lvquang <levinhquang1412@gmail.com>
2022-10-02 07:35:09 +0700

    add more custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        "./layouts/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
@@ -16,6 +17,13 @@ module.exports = {
                 button: "rgba(0, 149, 246, 0.3)",
                 dash: "#dbdbdb",
                 textGray: "#8e8e8e",
+                textDarkGray: "#262626",
+                redBadge: "rgb(255, 48, 65)",
+                errorOrDestructive: "rgba(237, 73, 86)",
+                overlay: "rgba(0, 0, 0, 0.65)",
+                142: "rgb(142, 142, 142)",
+                38: "rgb(38, 38, 38)",
+                250: "rgb(250, 250, 250)",
             },
         },
     },
